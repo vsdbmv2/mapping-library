b6470fd60b63ac3fe5e73355f22e46759645cc85
Helton Fábio <hfabio@upsiide.com>
2022-05-22 13:43:24 -0300

    changing export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import { epitopeMap } from '.';
+import { smithWaterman } from '.';
+import { neoNeedlemanGotoh } from '.';
 export { default as epitopeMap } from "./epitopeMap";
 export { default as smithWaterman } from "./smithWaterman";
 export { default as neoNeedlemanGotoh } from "./neoNeedlemanGotoh";
+
+export default {epitopeMap, smithWaterman, neoNeedlemanGotoh}
